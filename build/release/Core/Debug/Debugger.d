dd8c07b772ef95deb516b69e0df40335f7b5d78a
imre <mc.imre@hotmail.com>
2020-03-09 21:44:50 +0100

    Added Gitignore

--- build/release/Core/Debug/Debugger.d
+++
@@ -1,9 +0,0 @@
-build/release/Core/Debug/Debugger.o: src/Core/Debug/Debugger.cpp \
- inc/Core/Debug/Debugger.hpp inc/Core/Debug/DefineDebug.h \
- Libraries/GL/glew.h
-
-inc/Core/Debug/Debugger.hpp:
-
-inc/Core/Debug/DefineDebug.h:
-
-Libraries/GL/glew.h:
